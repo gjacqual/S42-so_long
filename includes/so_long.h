e909eb657ee46fd5882bdecc17537f0a11bd28e1
gjacqual <gjacqual@student.21-school.ru>
2022-01-18 16:26:01 +0300

    update

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: gjacqual <gjacqual@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 03:48:02 by gjacqual          #+#    #+#             */
-/*   Updated: 2022/01/18 00:32:24 by gjacqual         ###   ########.fr       */
+/*   Updated: 2022/01/18 16:22:09 by gjacqual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,29 @@
 # include <string.h>
 # include "../libft/libft.h"
 # include "../mlx-linux/mlx.h"
+
+typedef struct s_vars {
+	void	*mlx;
+	void	*mlx_win;
+}				t_vars;
+
+typedef struct s_image {
+	void	*grass;
+	void	*rock;
+	void	*exit;
+	void	*orb;
+	void	*player;
+	void	*enemy;
+	int		img_width;
+	int		img_height;
+}			t_image;
+
+typedef struct s_data {
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 void	error(char *message);
 int		check_map_path(char *str);
--- srcs/init.c
+++ srcs/init.c
@@ -6,31 +6,11 @@
 /*   By: gjacqual <gjacqual@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 00:25:30 by gjacqual          #+#    #+#             */
-/*   Updated: 2022/01/18 06:54:02 by gjacqual         ###   ########.fr       */
+/*   Updated: 2022/01/18 16:21:37 by gjacqual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-typedef struct s_vars {
-	void	*mlx;
-	void	*mlx_win;
-}	t_vars;
-
-typedef struct s_data {
-	void	*img;
-	void	*img2;
-	void	*img3;
-	void	*img4;
-	void	*img5;
-	void	*img6;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-	int		img_width;
-	int		img_height;
-}	t_data;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -60,7 +40,7 @@ void	xwindow_init(void)
 	t_vars	vars;
 	
 	// void	*img;
-	t_data	img;
+	t_image img;
 	char	*sprite_grass = "./images/grass.xpm";
 	char	*sprite_rock = "./images/rock.xpm";
 	char	*sprite_chest = "./images/chest.xpm";
@@ -76,21 +56,21 @@ void	xwindow_init(void)
 	vars.mlx_win = mlx_new_window(vars.mlx, 800, 600, "So_long Gjacqual");
 	
 	// img.img = mlx_new_image(vars.mlx, 800, 600);
-	img.img = mlx_xpm_file_to_image(vars.mlx, sprite_grass, &img.img_width, &img.img_height);
-	img.img2 = mlx_xpm_file_to_image(vars.mlx, sprite_rock, &img.img_width, &img.img_height);
-	img.img3 = mlx_xpm_file_to_image(vars.mlx, sprite_chest, &img.img_width, &img.img_height);
-	img.img4 = mlx_xpm_file_to_image(vars.mlx, sprite_orb, &img.img_width, &img.img_height);
-	img.img5 = mlx_xpm_file_to_image(vars.mlx, sprite_player, &img.img_width, &img.img_height);
-	img.img6 = mlx_xpm_file_to_image(vars.mlx, sprite_skelet, &img.img_width, &img.img_height);
+	img.grass = mlx_xpm_file_to_image(vars.mlx, sprite_grass, &img.img_width, &img.img_height);
+	img.rock = mlx_xpm_file_to_image(vars.mlx, sprite_rock, &img.img_width, &img.img_height);
+	img.exit = mlx_xpm_file_to_image(vars.mlx, sprite_chest, &img.img_width, &img.img_height);
+	img.orb = mlx_xpm_file_to_image(vars.mlx, sprite_orb, &img.img_width, &img.img_height);
+	img.player = mlx_xpm_file_to_image(vars.mlx, sprite_player, &img.img_width, &img.img_height);
+	img.enemy = mlx_xpm_file_to_image(vars.mlx, sprite_skelet, &img.img_width, &img.img_height);
 	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	// my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
 	
-	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.img, 30, 0);
-	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.img2, 50, 50);
-	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.img3, 100, 100);
-	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.img4, 150, 150);
-	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.img5, 200, 200);
-	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.img6, 250, 250);
+	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.grass, 30, 0);
+	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.rock, 50, 50);
+	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.exit, 100, 100);
+	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.orb, 150, 150);
+	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.player, 200, 200);
+	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.enemy, 250, 250);
 
 	mlx_string_put(vars.mlx, vars.mlx_win, 10, 15, 0x005616, "Move count:");
 	
