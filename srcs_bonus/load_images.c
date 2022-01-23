d1a383451655621ef22bf1752d6cc0e24f1c7660
gjacqual <gjacqual@student.21-school.ru>
2022-01-23 23:44:06 +0300

    add player images

--- srcs_bonus/load_images.c
+++ srcs_bonus/load_images.c
@@ -6,15 +6,32 @@
 /*   By: gjacqual <gjacqual@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 03:23:04 by gjacqual          #+#    #+#             */
-/*   Updated: 2022/01/23 06:27:38 by gjacqual         ###   ########.fr       */
+/*   Updated: 2022/01/23 23:40:00 by gjacqual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
+
+static void	load_player_images(t_game	*game)
+{
+	game->img.player = mlx_xpm_file_to_image(\
+	game->mlx, IMGPATH PLAYER_F, &game->img.img_width, &game->img.img_height);
+	game->img.player_up = mlx_xpm_file_to_image(\
+	game->mlx, IMGPATH PLAYER_B, &game->img.img_width, &game->img.img_height);
+	game->img.player_le = mlx_xpm_file_to_image(\
+	game->mlx, IMGPATH PLAYER_L, &game->img.img_width, &game->img.img_height);
+	game->img.player_ri = mlx_xpm_file_to_image(\
+	game->mlx, IMGPATH PLAYER_R, &game->img.img_width, &game->img.img_height);
+	game->img.win = mlx_xpm_file_to_image(\
+	game->mlx, IMGPATH PLAYER_WIN, &game->img.img_width, &game->img.img_height);
+}
+
+
 /* Upload all images to the game */
 void	load_images(t_game	*game)
 {
+	load_player_images(game);
 	game->img.grass = mlx_xpm_file_to_image(\
 	game->mlx, IMGPATH GRASS, &game->img.img_width, &game->img.img_height);
 	game->img.rock = mlx_xpm_file_to_image(\
@@ -25,8 +42,7 @@ void	load_images(t_game	*game)
 	game->mlx, IMGPATH EXIT_CL, &game->img.img_width, &game->img.img_height);
 	game->img.orb = mlx_xpm_file_to_image(\
 	game->mlx, IMGPATH COLLECT, &game->img.img_width, &game->img.img_height);
-	game->img.player = mlx_xpm_file_to_image(\
-	game->mlx, IMGPATH PLAYER, &game->img.img_width, &game->img.img_height);
-	game->img.win = mlx_xpm_file_to_image(\
-	game->mlx, IMGPATH PLAYER_WIN, &game->img.img_width, &game->img.img_height);
 }
+
+
+
